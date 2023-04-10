3ea78922231464b68d7d57075533bc54ce875cbb
dhananjaya das <dhananjaya.das@appman.in>
2023-04-11 00:10:04 +0530

    Department Service

---
+++ Library/CloudVOffice.Services/HR/Master/DepartmentService.cs
@@ -0,0 +1,146 @@
+﻿using CloudVOffice.Core.Domain.HR.Master;
+using CloudVOffice.Data.DTO.HR.Master;
+using CloudVOffice.Data.Persistence;
+using CloudVOffice.Data.Repository;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.HR.Master
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly ApplicationDBContext _dbContext;
+        private readonly ISqlRepository<Department> _departmentRepo;
+        public DepartmentService(ApplicationDBContext dbContext, ISqlRepository<Department> departmentRepo)
+        {
+
+            _dbContext = dbContext;
+            _departmentRepo = departmentRepo;
+        }
+        public string CreateDepartment(DepartmentDTO departmentDTO)
+        {
+            try
+            {
+                var department = _dbContext.Departments.Where(x => x.DepartmentName == departmentDTO.DepartmentName && x.Deleted == false && x.Parent==departmentDTO.Parent);
+                if (department == null)
+                {
+                    _departmentRepo.Insert(new Department()
+                    {
+                        DepartmentName = departmentDTO.DepartmentName,
+                        DepartmentId = departmentDTO.DepartmentId,
+                        Parent = departmentDTO.Parent,
+                        IsGroup = departmentDTO.IsGroup,
+                        CreatedBy = departmentDTO.CreatedBy,
+                        CreatedDate = DateTime.Now,
+                        Deleted = false,
+                    });
+                    return "Success";
+                }
+                else return "duplicate";
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public string DepartmentDelete(int deprtmentid, int DeletedBy)
+        {
+            try
+            {
+                var a = _dbContext.Departments.Where(x => x.DepartmentId == deprtmentid).FirstOrDefault();
+                if (a != null)
+                {
+                    a.Deleted = true;
+                    a.UpdatedBy = DeletedBy;
+                    a.UpdatedDate = DateTime.Now;
+                    _dbContext.SaveChanges();
+                    return "deleted";
+                }
+                else
+                    return "invalid";
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public string DepartmentUpdate(DepartmentDTO departmentDTO)
+        {
+
+            try
+            {
+                var department = _dbContext.Departments.Where(x => x.DepartmentId != departmentDTO.DepartmentId && x.DepartmentName == departmentDTO.DepartmentName && x.Deleted == false & x.Parent==departmentDTO.Parent).FirstOrDefault();
+                if (department == null)
+                {
+                    var a = _dbContext.Departments.Where(x => x.DepartmentId == departmentDTO.DepartmentId).FirstOrDefault();
+                    if (a != null)
+                    {
+                        a.DepartmentName = departmentDTO.DepartmentName;
+                        a.Parent = departmentDTO.Parent;
+                        a.IsGroup = departmentDTO.IsGroup;
+                        a.UpdatedBy = departmentDTO.CreatedBy;
+                        a.UpdatedDate = DateTime.Now;
+                        _dbContext.SaveChanges();
+                        return "delete";
+                    }
+                    else
+                        return "invalid";
+                }
+                else
+                {
+                    return "duplicate";
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Department> GetAllDepartmentGroups()
+        {
+            try
+            {
+                return _dbContext.Departments.Where(x => x.IsGroup == true && x.Deleted ==false).ToList();
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Department GetDepartmentById(int departmentId)
+        {
+            try
+            {
+                return _dbContext.Departments.Where(x => x.DepartmentId == departmentId && x.Deleted == false).SingleOrDefault();
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Department> GetDepartmentList()
+        {
+            try
+            {
+                return _dbContext.Departments.Where(x => x.Deleted == false).ToList();
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Library/CloudVOffice.Services/HR/Master/IDepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using CloudVOffice.Core.Domain.HR.Master;
+using CloudVOffice.Data.DTO.HR.Master;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.HR.Master
+{
+    public interface IDepartmentService
+    {
+        public string CreateDepartment(DepartmentDTO departmentDTO);
+        public List<Department> GetDepartmentList();
+        public Department GetDepartmentById(int departmentId);
+        public string DepartmentUpdate(DepartmentDTO departmentDTO);
+        public string DepartmentDelete(int deprtmentid, int DeletedBy);
+        public List<Department> GetAllDepartmentGroups(); 
+    }
+}

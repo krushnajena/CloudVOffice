21334e1d76206fafd09ce46b784b8716265df8d7
deepak <deepak.panigrahi@ajatus.co.in>
2023-04-12 01:09:20 +0530

    Branch Create

--- Plugin/HR.Base/Controllers/BranchController.cs
+++ Plugin/HR.Base/Controllers/BranchController.cs
@@ -1,8 +1,11 @@
-﻿using CloudVOffice.Services.HR.Master;
+﻿using CloudVOffice.Core.Domain.HR.Master;
+using CloudVOffice.Data.DTO.HR.Master;
+using CloudVOffice.Services.HR.Master;
 using CloudVOffice.Services.Users;
 using CloudVOffice.Web.Framework;
 using CloudVOffice.Web.Framework.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +21,75 @@ namespace HR.Base.Controllers
         
             _branchService= branchService;
         }
+        [HttpGet]
+        public IActionResult BranchCreate(int? branchId)
+        {
+            BranchDTO branchDTO = new BranchDTO();
+            //ViewBag.ParentDepartmentList = new SelectList(_branchService.GetBranches(), "BranchId", "BranchName");
+            if (branchId != null)
+            {
+
+                Branch d = _branchService.GetBranchByBranchId(int.Parse(branchId.ToString()));
+
+                branchDTO.BranchName = d.BranchName;
+              
+            }
+
+            return View("~/Plugins/HR.Base/Views/Branch/BranchCreate.cshtml", branchDTO);
+
+        }
+
+        [HttpPost]
+        public IActionResult BranchCreate(BranchDTO branchDTO)
+        {
+			branchDTO.CreatedBy = (int)Int64.Parse(User.Claims.FirstOrDefault(x => x.Type == "UserId").Value.ToString());
+
+
+			if (ModelState.IsValid)
+            {
+                if (branchDTO.BranchId == null)
+                {
+                    var a = _branchService.BranchCreate(branchDTO);
+                    if (a == "Success")
+                    {
+                        return Redirect("/HR/Branch/BranchList");
+                    }
+                    else if (a == "duplicate")
+                    {
+                        ModelState.AddModelError("", "Branch Already Exists");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Un-Expected Error");
+                    }
+                }
+                else
+                {
+                    var a = _branchService.BranchUpdate(branchDTO);
+                    if (a == "success")
+                    {
+
+                    }
+                    else if (a == "duplicate")
+                    {
+                        ModelState.AddModelError("", "Branch Already Exists");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Un-Expected Error");
+                    }
+                }
+            }
+            //ViewBag.ParentDepartmentList = new SelectList((System.Collections.IEnumerable)_departmentService.GetAllDepartmentGroups(), "DepartmentId", "DepartmentName");
+
+            return View("~/Plugins/HR.Base/Views/Branch/BranchCreate.cshtml", branchDTO);
+        }
 
         public IActionResult BranchView()
         {
             ViewBag.branches = _branchService.GetBranches();
 
-            return View("~/Plugins/View/Branch/BranchView.cshtml");
+            return View("~/Plugins/HR.Base/Views/Branch/BranchView.cshtml");
         }
 
     }

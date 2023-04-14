ff7d9c2f7d194d78d710eb777b1c0ca6b7860a3e
mohanbarik <mohan.barik@appman.in>
2023-04-14 13:29:00 +0530

    Designation

--- Library/CloudVOffice.Services/HR/Master/DesignationService.cs
+++ Library/CloudVOffice.Services/HR/Master/DesignationService.cs
@@ -4,6 +4,7 @@ using CloudVOffice.Data.DTO.HR.Master;
 using CloudVOffice.Data.Persistence;
 using CloudVOffice.Data.Repository;
 using Microsoft.CodeAnalysis.Operations;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,124 +15,120 @@ namespace CloudVOffice.Services.HR.Master
 {
     public class DesignationService : IDesignationService
     {
-        private readonly ApplicationDBContext _dbContext;
-        private readonly ISqlRepository<Designation> _designationRepo;
-        public DesignationService(ApplicationDBContext dbContext, ISqlRepository<Designation> designationRepo)
-        {
+		private readonly ApplicationDBContext _Context;
+		private readonly ISqlRepository<Designation> _designationRepo;
+		public DesignationService(ApplicationDBContext Context, ISqlRepository<Designation> designationRepo)
+		{
 
-            _dbContext = dbContext;
-            _designationRepo = designationRepo;
-        }
-        public MennsageEnum CreateDesignation(DesignationDTO designationDTO)
-        {
+			_Context = Context;
+			_designationRepo = designationRepo;
+		}
 
-            try
-            {
-                var brach = _dbContext.Designations.Where(x => x.DesignationName == designationDTO.DesignationName && x.Deleted == false).FirstOrDefault();
-                if (brach == null)
-                {
-                    _designationRepo.Insert(new Designation()
-                    {
-                        DesignationName = designationDTO.DesignationName,
-                        Description = designationDTO.Description,
-                        CreatedBy = designationDTO.CreatedBy,
-                        CreatedDate = DateTime.Now,
-                        Deleted = false
-                    });
-                    return MennsageEnum.Success;
-                }
-                else
-                    return MennsageEnum.Duplicate;
-            }
-            catch
-            {
-                throw;
-            }
-        }
+		public MennsageEnum CreateDesignation(DesignationDTO designationDTO)
+		{
+			try
+			{
+				var brach = _Context.Designations.Where(x => x.DesignationName == designationDTO.DesignationName && x.Deleted == false).FirstOrDefault();
+				if (brach == null)
+				{
+					_designationRepo.Insert(new Designation()
+					{
+						DesignationName = designationDTO.DesignationName,
+						Description= designationDTO.Description,
+						CreatedBy = designationDTO.CreatedBy,
+						CreatedDate = DateTime.Now,
+						Deleted = false
+					});
+					return MennsageEnum.Success;
+				}
+				else
+					return MennsageEnum.Duplicate;
+			}
+			catch
+			{
+				throw;
+			}
 
-        public MennsageEnum DesignationDelete(int designationId, int DeletedBy)
-        {
-            try
-            {
-                var a = _dbContext.Designations.Where(x => x.DesignationId == designationId).FirstOrDefault();
-                if (a != null)
-                {
-                    a.Deleted = true;
-                    a.UpdatedBy = DeletedBy;
-                    a.UpdatedDate = DateTime.Now;
-                    _dbContext.SaveChanges();
-                    return MennsageEnum.Deleted;
-                }
-                else
-                    return MennsageEnum.Invalid;
-            }
-            catch
-            {
-                throw;
-            }
-        }
+		}
 
-        public MennsageEnum DesignationUpdate(DesignationDTO designationDTO)
-        {
-            try
-            {
-                var branch = _dbContext.Designations.Where(x => x.DesignationId != designationDTO.DesignationId && x.DesignationName == designationDTO.DesignationName && x.Deleted == false).FirstOrDefault();
-                if (branch == null)
-                {
-                    var a = _dbContext.Designations.Where(x => x.DesignationId == designationDTO.DesignationId).FirstOrDefault();
-                    if (a != null)
-                    {
-                        a.DesignationName = designationDTO.DesignationName;
-                        a.Description = designationDTO.Description;
-                        a.UpdatedBy = designationDTO.CreatedBy;
-                        a.UpdatedDate = DateTime.Now;
-                        _dbContext.SaveChanges();
-                        return MennsageEnum.Updated;
-                    }
-                    else
-                        return MennsageEnum.Invalid;
-                }
-                else
-                {
-                    return MennsageEnum.Duplicate;
-                }
+		public MennsageEnum DesignationDelete(Int64 designationId, Int64 DeletedBy)
+		{
+			try
+			{
+				var a = _Context.Designations.Where(x => x.DesignationId == designationId).FirstOrDefault();
+				if (a != null)
+				{
+					a.Deleted = true;
+					a.UpdatedBy = DeletedBy;
+					a.UpdatedDate = DateTime.Now;
+					_Context.SaveChanges();
+					return MennsageEnum.Deleted;
+				}
+				else
+					return MennsageEnum.Invalid;
+			}
+			catch
+			{
+				throw;
+			}
+		}
 
-            }
-            catch
-            {
-                throw;
-            }
-        }
+		public MennsageEnum DesignationUpdate(DesignationDTO designationDTO)
+		{
+			try
+			{
+				var branch = _Context.Designations.Where(x => x.DesignationId != designationDTO.DesignationId && x.DesignationName == designationDTO.DesignationName && x.Deleted == false).FirstOrDefault();
+				if (branch == null)
+				{
+					var a = _Context.Designations.Where(x => x.DesignationId == designationDTO.DesignationId).FirstOrDefault();
+					if (a != null)
+					{
+						a.DesignationName = designationDTO.DesignationName;
+						a.Description = designationDTO.Description;
+						a.UpdatedBy = designationDTO.CreatedBy;
+						a.UpdatedDate = DateTime.Now;
+						_Context.SaveChanges();
+						return MennsageEnum.Updated;
+					}
+					else
+						return MennsageEnum.Invalid;
+				}
+				else
+				{
+					return MennsageEnum.Duplicate;
+				}
 
-        public Designation GetDesignationById(int designationId)
-        {
-            try
-            {
-                return _dbContext.Designations.Where(x => x.DesignationId == designationId && x.Deleted == false).SingleOrDefault();
+			}
+			catch
+			{
+				throw;
+			}
+		}
 
-            }
-            catch
-            {
-                throw;
-            }
-        }
+		public Designation GetDesignationById(Int64 designationId)
+		{
+			try
+			{
+				return _Context.Designations.Where(x => x.DesignationId == designationId && x.Deleted == false).SingleOrDefault();
 
-        public List<Designation> GetDesignationList()
-        {
-            try
-            {
-                return _dbContext.Designations.Where(x => x.Deleted == false).ToList();
+			}
+			catch
+			{
+				throw;
+			}
+		}
 
-            }
-            catch
-            {
-                throw;
-            }
-        }
+		public List<Designation> GetDesignationList()
+		{
+			try
+			{
+				return _Context.Designations.Where(x => x.Deleted == false).ToList();
 
-        MennsageEnum IDesignationService.DesignationUpdate(DesignationDTO designationDTO)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			}
+			catch
+			{
+				throw;
+			}
+		}
+	}
 }

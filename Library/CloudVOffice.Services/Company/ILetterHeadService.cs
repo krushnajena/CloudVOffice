d3364aae8cf742689227f8bc0ab4688bf392ddd7
Admin <Admin@DESKTOP-6OFD72I>
2023-05-21 13:21:19 +0530

    letterhead

---
+++ Library/CloudVOffice.Services/Company/ILetterHeadService.cs
@@ -0,0 +1,21 @@
+﻿using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.Company;
+using CloudVOffice.Core.Domain.Projects;
+using CloudVOffice.Data.DTO.Company;
+using CloudVOffice.Data.DTO.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.Company
+{
+	public interface ILetterHeadService
+	{
+		public MennsageEnum LetterHeadCreate(LetterHeadDTO letterHeadDTO);
+		public List<LetterHead> GetLetterHeads();
+		public LetterHead GetLetter();
+		
+	}
+}

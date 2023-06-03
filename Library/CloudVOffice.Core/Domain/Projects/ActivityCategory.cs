6889bb9e3110a2062840097db8ec1f45aa66cc4a
Krushna Jena <krushnajena@appman.in>
2023-06-03 18:22:07 +0530

    Timesheet Activity Categogry

---
+++ Library/CloudVOffice.Core/Domain/Projects/ActivityCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Core.Domain.Projects
+{
+    public class TimesheetActivityCategory : IAuditEntity, ISoftDeletedEntity
+    {
+        public int TimesheetActivityCategoryId { get; set; }
+        public string TimesheetActivityCategoryName { get;set; }
+        public Int64 CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Int64? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool Deleted { get; set; }
+
+    }
+}

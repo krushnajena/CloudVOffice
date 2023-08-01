a6e5da0dfdc294cbc1ba914c0ca10a4abe358e78
mohanbarik <mohan.barik@appman.in>
2023-08-01 11:33:15 +0530

    AttendanceDevice

---
+++ Library/CloudVOffice.Services/Attendance/IAttendanceDeviceService.cs
@@ -0,0 +1,15 @@
+﻿using CloudVOffice.Core.Domain.Attendance;
+using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Data.DTO.Attendance;
+
+namespace CloudVOffice.Services.Attendance
+{
+	public interface IAttendanceDeviceService
+	{
+		public MessageEnum CreateAttendanceDevice(AttendanceDeviceDTO attendanceDeviceDTO);
+		public List<AttendanceDevice> GetAttendanceDeviceList();
+		public AttendanceDevice GetAttendanceDeviceById(int attendanceDeviceId);
+		public MessageEnum AttendanceDeviceUpdate(AttendanceDeviceDTO attendanceDeviceDTO);
+		public MessageEnum AttendanceDeviceDelete(int attendanceDeviceId, Int64 DeletedBy);
+	}
+}

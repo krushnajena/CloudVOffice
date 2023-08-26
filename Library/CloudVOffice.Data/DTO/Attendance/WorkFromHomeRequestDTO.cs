a806b7ee7340bc2e4abdb6f7bc00ea5589b3d052
dhananjaya das <dhananjaya.das@appman.in>
2023-08-26 18:47:11 +0530

    WorkFormHomeRequest

---
+++ Library/CloudVOffice.Data/DTO/Attendance/WorkFromHomeRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Data.DTO.Attendance
+{
+    public class WorkFromHomeRequestDTO
+    {
+        public Int64 WorkFromHomeRequestId { get; set; }
+        public Int64 EmployeeId { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public string Reason { get; set; }
+        public int ApprovalStatus { get; set; } //0 - Submitted, 1- Approved , 2- Rejected
+        public Int64 ApprovedBy { get; set; }
+        public DateTime? ApprovedDate { get; set; }
+        public string ApprovalRemark { get; set; }
+        public Int64 CreatedBy { get; set; }
+    }
+}
---
+++ Library/CloudVOffice.Services/Attendance/IWorkFromHomeRequestService.cs
@@ -0,0 +1,21 @@
+﻿using CloudVOffice.Core.Domain.Attendance;
+using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.HR.Attendance;
+using CloudVOffice.Data.DTO.Attendance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.Attendance
+{
+    public interface IWorkFromHomeRequestService
+    {
+        public MessageEnum WorkFromHomeRequestCreate(WorkFromHomeRequestDTO workFromHomeRequestDTO);
+        public List<WorkFromHomeRequest> GetWorkFromHomeRequestList();
+        public WorkFromHomeRequest GetWorkFromHomeRequestById(Int64 workFromHomeRequestId);
+        public MessageEnum WorkFromHomeRequestUpdate(WorkFromHomeRequestDTO workFromHomeRequestDTO);
+        public MessageEnum WorkFromHomeRequestDelete(Int64 workFromHomeRequestId, Int64 DeletedBy);
+    }
+}

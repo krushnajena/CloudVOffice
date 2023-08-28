d9a3122159ef2ad56b0a71e6c09b8b6cd2281685
Krushna Jena <krushnajena@appman.in>
2023-08-28 12:04:39 +0530

    wfh

--- Library/CloudVOffice.Core/Domain/Attendance/WorkFromHomeRequest.cs
+++ Library/CloudVOffice.Core/Domain/Attendance/WorkFromHomeRequest.cs
@@ -16,9 +16,9 @@ namespace CloudVOffice.Core.Domain.Attendance
 		public DateTime? ToDate { get; set;}
 		public string Reason { get; set; }	
 		public int ApprovalStatus { get; set; } //0 - Submitted, 1- Approved , 2- Rejected
-		public Int64 ApprovedBy { get; set; }
+		public Int64? ApprovedBy { get; set; }
 		public DateTime? ApprovedDate { get; set; }
-		public string ApprovalRemark { get; set; }
+		public string? ApprovalRemark { get; set; }
 		public Int64 CreatedBy { get; set; }
 		public DateTime CreatedDate { get; set; }
 		public Int64? UpdatedBy { get; set; }

017ea9947f60ade0efb933907aeb677b06c62f3b
dhananjaya das <dhananjaya.das@appman.in>
2023-09-01 16:26:23 +0530

    InterviewPanelMember

---
+++ Library/CloudVOffice.Services/Recruitment/IInterviewPanelMemberService.cs
@@ -0,0 +1,20 @@
+﻿using CloudVOffice.Core.Domain.Common;
+using CloudVOffice.Core.Domain.Recruitment;
+using CloudVOffice.Data.DTO.Recruitment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Services.Recruitment
+{
+	public interface IInterviewPanelMemberService
+	{
+		public MessageEnum InterviewPanelMemberCreate(InterviewPanelMemberDTO interviewPanelMemberDTO);
+		public List<InterviewPanelMember> GetInterviewPanelMemberList();
+		public InterviewPanelMember GetInterviewPanelMemberById(int interviewPanelMemberId);
+		public MessageEnum InterviewPanelMemberUpdate(InterviewPanelMemberDTO interviewPanelMemberDTO);
+		public MessageEnum InterviewPanelMemberDelete(int interviewPanelMemberId, Int64 DeletedBy);
+	}
+}

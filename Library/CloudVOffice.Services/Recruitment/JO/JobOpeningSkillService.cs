2bf18799b8744da6458df20dec68f957d1338f63
Krushna Jena <krushnajena@appman.in>
2023-09-26 19:34:03 +0530

    Candidate

--- Library/CloudVOffice.Services/Recruitment/JO/JobOpeningSkillService.cs
+++ Library/CloudVOffice.Services/Recruitment/JO/JobOpeningSkillService.cs
@@ -22,14 +22,32 @@ namespace CloudVOffice.Services.Recruitment.JO
         }
         public MessageEnum CreateJobOpeningSkill(int JobId, int SkillId, Int64 createdBy)
         {
-            JobOpeningSkill skill =  new JobOpeningSkill();
-            skill.JobId = JobId;
-            skill.SkillId = SkillId;
-            skill.CreatedBy = createdBy;
-            skill.CreatedDate = DateTime.Now;
-            _jobOpeningRepo.Insert(skill);  
+            var a = _Context.JobOpeningSkills.Where(x => x.SkillId == SkillId && x.JobId == JobId && x.Deleted == false).ToList();
+            if (a.Count ==0) {
+                JobOpeningSkill skill = new JobOpeningSkill();
+                skill.JobId = JobId;
+                skill.SkillId = SkillId;
+                skill.CreatedBy = createdBy;
+                skill.CreatedDate = DateTime.Now;
+                _jobOpeningRepo.Insert(skill);
+            }
+            
             return MessageEnum.Success;
 
+        }
+
+        public List<JobOpeningSkill> RemoveUnListedSkills(List<int> skills,int jobId,Int64 updatedBy)
+        {
+            var a = _Context.JobOpeningSkills.Where(x => x.Deleted == false && !skills.Contains(x.SkillId) && x.JobId == jobId).ToList();
+            for(int i = 0; i < a.Count; i++)
+            {
+                var skill = a[i];
+                skill.Deleted = true;
+                skill.UpdatedDate= DateTime.Now;
+                skill.UpdatedBy = updatedBy;
+                _Context.SaveChanges();
+            }
+            return a;
         }
     }
 }

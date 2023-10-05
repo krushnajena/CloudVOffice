ff6ae581f79a48bb5b69f11efa6dc225ab25d59c
Pravat Kumar Behera <pravat.behera@appman.tech>
2023-10-05 10:39:26 +0530

    Pravat Chng Accounting

---
+++ Library/CloudVOffice.Data/DTO/Accounts/CustomerDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace CloudVOffice.Data.DTO.Custom
+{
+    public class CustomerDTO
+    {
+        public Int64? CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int? CustomerGroupId { get; set; }
+        public string TaxId { get; set; }
+        public Int64? AccountManagerId { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNo { get; set; }
+        public string EmailId { get; set; }
+        public string ContactPersonName { get; set; }
+        public string ContactPersonPhone { get; set; }
+        public string ContactPersonEmailId { get; set; }
+        public Int64 CreatedBy { get; set; }
+    }
+}
---
+++ Library/CloudVOffice.Data/DTO/Accounts/SupplierGroupDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Data.DTO.Accounts
+{
+	public class SupplierGroupDTO
+	{
+		public int SupplierGroupId { get; set; }
+		public string SupplierGroupName { get; set; }
+		public Int64 CreatedBy { get; set; }
+	}
+}

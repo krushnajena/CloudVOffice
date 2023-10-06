e80e46a608d0be52c34d7b23ec6a1e63a26ab638
Pravat Kumar Behera <pravat.behera@appman.tech>
2023-10-06 15:44:17 +0530

    Pravat Chng 06/10/23

---
+++ Library/CloudVOffice.Core/Domain/Accounts/Bank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudVOffice.Core.Domain.Accounts
+{
+    public class Bank
+    {
+        public Int64 BankId { get; set; }
+        public string BankName { get; set; }
+        public string? SwiftNo { get; set;}
+        public Int64 CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Int64? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
